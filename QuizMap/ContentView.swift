c04fce7e3a909d5509149e71bcdaac5c34a03761
Alexander von Below <alexander.von_below@telekom.de>
2020-09-18 13:46:06 +0200

    Initial Commit with default template file

---
+++ QuizMap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuizMap
+//
+//  Created by Alexander v. Below on 18.09.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuizMap/QuizMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizMapApp.swift
+//  QuizMap
+//
+//  Created by Alexander v. Below on 18.09.20.
+//
+
+import SwiftUI
+
+@main
+struct QuizMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
